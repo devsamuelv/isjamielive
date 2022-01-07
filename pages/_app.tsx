cdd924914055b367b23f38cb368d7bb9bca36f26
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2022-01-06 20:13:40 -0500

    Added splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<script async src="https://cdn.splitbee.io/sb.js"></script>
+			</Head>
+
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
